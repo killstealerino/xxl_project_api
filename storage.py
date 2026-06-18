b91754f6be7167253bc3d15b3abe7978b4d4f7d9
unknown <oleg20088002@gmail.com>
2026-06-18 14:30:08 +0200

    made basic functions in storage.py

---
+++ storage.py
@@ -0,0 +1,11 @@
+import json
+
+events_path = "data\events.json"
+
+def load_events():
+    with open(events_path, "r", encoding="utf-8") as reading_file:
+        return json.load(reading_file)
+
+def save_events(events):
+    with open(events_path, "w", encoding="utf-8") as reading_file:
+        json.dump(events, file, indent=4, ensure_ascii=False)
